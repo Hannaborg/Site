5b3841562d5cc0a8fef77b1129a1b758682d5292
Hanna Borg <hannaborg@macbookair.mynetworksettings.com>
2025-06-07 20:26:59 -0400

    init commit

---
+++ src/build.js
@@ -0,0 +1,40 @@
+const fs = require('fs-extra');
+const path = require('path');
+const { marked } = require('marked');
+
+// Ensure directories exist
+fs.ensureDirSync('src/content');
+fs.ensureDirSync('public');
+
+// Read the template
+const template = fs.readFileSync('src/template.html', 'utf-8');
+
+// Convert markdown to HTML
+function convertMarkdownToHtml(markdown) {
+    return marked.parse(markdown);
+}
+
+// Process all markdown files
+async function build() {
+    // Read all markdown files
+    const files = await fs.readdir('src/content');
+    
+    for (const file of files) {
+        if (file.endsWith('.md')) {
+            const content = await fs.readFile(path.join('src/content', file), 'utf-8');
+            const html = convertMarkdownToHtml(content);
+            
+            // Replace content in template
+            const pageHtml = template.replace('{{content}}', html);
+            
+            // Write to public directory
+            const outputFile = file.replace('.md', '.html');
+            await fs.writeFile(path.join('public', outputFile), pageHtml);
+        }
+    }
+    
+    // Copy static assets
+    await fs.copy('src/static', 'public', { overwrite: true });
+}
+
+build().catch(console.error); 
---
+++ src/content/index.md
@@ -0,0 +1,16 @@
+# Welcome to My Site
+
+This is a simple, clean website built with HTML, CSS, and JavaScript. I write about technology, programming, and other interesting topics.
+
+## Latest Posts
+
+- [Getting Started with Web Development](/blog/getting-started.html)
+- [Why I Love Markdown](/blog/markdown-love.html)
+
+## About Me
+
+I'm a passionate developer who loves creating simple, effective solutions. Check out my [About](/about.html) page to learn more about me and my work.
+
+## Get in Touch
+
+Have questions? Check out the [FAQ](/faq.html) or [contact me](/contact.html) directly. 
